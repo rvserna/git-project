4fc18a383c411867c39dfbe2e6ae60a709ef7b85
rvserna <viensmrobert@gmail.com>
2024-04-13 13:00:40 -0400

    First Commit

---
+++ test.py
@@ -0,0 +1,73 @@
+"""Examples of functions"""
+
+greek_island = "Santorini"
+
+print(greek_island)
+
+earth_age_bln = 4.4
+universe_age_bln = 14
+earth_age_bln += 0.1
+
+print (earth_age_bln)
+
+asia_wishlist = [
+    "Bhutan",
+    "Ha Long",
+    "Laos",
+    "Danxia",
+    "Seoul",
+    "Khao Sok,"
+    "Cebu",
+    "Chiang Mai",
+    "Ho Chi Minh"
+    ]
+
+msg = "life is beautiful"
+if msg == "I love you":
+    print ("propose")
+else:
+    print ("wait xP")
+
+net_earnings = 10000000
+if net_earnings >= 100000000:
+    print ("Large Cap")
+else:
+    print ("Small Cap")
+
+first = [
+    "soccer",
+    "swimming",
+    "running",
+    "skiing"
+    ]
+if "rock climbing" not in first:
+    print ("boo")
+
+web_data = ["techresearch and computervision"]
+if "@" in web_data:
+    print ("email address")
+elif "0123456789" in web_data:
+    print ("phone number")
+else:
+    print ("not e-mail nor phone number")
+
+a = 10+20
+b = 100 - 1
+c = 50 / 7
+d = 50 // 7
+e = 10 % 8
+f = 5 ** 2
+
+print (a,b,c,d,e,f, sep='\n')
+
+
+
+
+
+
+
+
+
+    
+
+
